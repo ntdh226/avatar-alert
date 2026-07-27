91b29cb99f0c1661c6ec7ebb3c3589c0b6b1a03b
ntdh226 <ngothidieuhien0202@gmail.com>
2026-07-27 12:10:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from playwright.sync_api import sync_playwright
 
-URL = "https://www.facebook.com/Avatar.CFS"
-
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-
-    page.goto(URL, wait_until="networkidle", timeout=60000)
+    page.goto("https://www.facebook.com/Avatar.CFS", wait_until="networkidle")
     page.wait_for_timeout(5000)
-
     print(page.locator("body").inner_text())
-
     browser.close()
